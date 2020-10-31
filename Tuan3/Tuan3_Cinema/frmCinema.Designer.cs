ad93a4ed9d79230b3e883fe896229b7a6f0461cd
QuocDongCao <fantasywhite97@gmail.com>
2020-11-01 01:51:11 +0700

    Them tuan 3

---
+++ Tuan3/Tuan3_Cinema/frmCinema.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Tuan3_Cinema
+{
+    partial class frmCinema
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.btnChon = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(565, 74);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(166, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MÀN HÌNH";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 92);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(565, 343);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 452);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "THÀNH TIỀN";
+            // 
+            // txtGia
+            // 
+            this.txtGia.Location = new System.Drawing.Point(206, 452);
+            this.txtGia.Multiline = true;
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(371, 31);
+            this.txtGia.TabIndex = 3;
+            // 
+            // btnChon
+            // 
+            this.btnChon.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChon.Location = new System.Drawing.Point(18, 495);
+            this.btnChon.Name = "btnChon";
+            this.btnChon.Size = new System.Drawing.Size(173, 38);
+            this.btnChon.TabIndex = 4;
+            this.btnChon.Text = "Chọn";
+            this.btnChon.UseVisualStyleBackColor = true;
+            this.btnChon.Click += new System.EventHandler(this.btnChon_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(206, 495);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(173, 38);
+            this.btnHuy.TabIndex = 4;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(404, 495);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Kết Thúc";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 555);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(589, 33);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "Tổng tiền:";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(99, 28);
+            this.toolStripStatusLabel1.Text = "Tổng tiền:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(23, 28);
+            this.toolStripStatusLabel2.Text = "0";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.toolStripStatusLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(199, 28);
+            this.toolStripStatusLabel3.Text = "toolStripStatusLabel3";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.toolStripStatusLabel4.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(199, 28);
+            this.toolStripStatusLabel4.Text = "toolStripStatusLabel4";
+            // 
+            // frmCinema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 588);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnChon);
+            this.Controls.Add(this.txtGia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmCinema";
+            this.Text = "Bán vé rạp phim";
+            this.Load += new System.EventHandler(this.frmCinema_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGia;
+        private System.Windows.Forms.Button btnChon;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+    }
+}
+
---
+++ Tuan3/Tuan3_Cinema/frmCinema.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tuan3_Cinema
+{
+    public partial class frmCinema : Form
+    {
+        public frmCinema()
+        {
+            InitializeComponent();
+        }
+        const int DAYA = 100;
+        const int DAYB = 200;
+        const int DAYC = 300;
+        
+        private void frmCinema_Load(object sender, EventArgs e)
+        {
+            CreateButton(15);
+            statusStrip1.Items[2].Text = "Ngày:" + DateTime.Now.ToString("dd/MM/yyyy");
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        public void CreateButton(int n)
+        {
+            Button btn;
+            for (int i= 1; i<= n;i++)
+            {
+                btn = new Button();
+                btn.Text = i.ToString();
+                btn.Name = "A" + i.ToString();
+                btn.Width = 107;
+                btn.Height =100;
+                btn.BackColor = Color.Gray;
+                btn.Click += new EventHandler(btnTuchon_Click);
+                flowLayoutPanel1.Controls.Add(btn);
+                
+            }
+        }
+
+        private void btnTuchon_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int tienchon = 0;
+            if (btn.BackColor != Color.Yellow)
+                btn.BackColor = Color.Yellow;
+            else
+                btn.BackColor = Color.Gray;
+            
+            tienchon = Tintiendangchon();
+            txtGia.Text = tienchon.ToString("#,##0");
+            
+        }
+
+        private int Tintiendangchon()
+        {
+            int Giatri = 0;
+            int tien = 0;
+            foreach (Button btn in flowLayoutPanel1.Controls)
+            {
+                if (btn.BackColor == Color.Yellow)
+                {
+                    Giatri = Convert.ToInt32(btn.Text);
+                    if (Giatri <= 5)
+                        tien += DAYA;
+                    else if (Giatri <= 10)
+                        tien += DAYB;
+                    else
+                        tien += DAYC;
+
+                }
+            }
+            return tien;
+        }
+        private int Tintongtien()
+        {
+            int Giatri = 0;
+            int tien = 0;
+            foreach (Button btn in flowLayoutPanel1.Controls)
+            {
+                if (btn.BackColor == Color.Aqua)
+                {
+                    Giatri = Convert.ToInt32(btn.Text);
+                    if (Giatri <= 5)
+                        tien += DAYA;
+                    else if (Giatri <= 10)
+                        tien += DAYB;
+                    else
+                        tien += DAYC;
+
+                }
+            }
+            return tien;
+        }
+        private void btnChon_Click(object sender, EventArgs e)
+        {
+            foreach(Button btn in flowLayoutPanel1.Controls)
+            {
+                if (btn.BackColor == Color.Yellow)
+                {
+                    
+                    btn.BackColor = Color.Aqua;
+                    btn.Enabled = false;
+                }
+                
+            }
+            txtGia.Text = "";
+            statusStrip1.Items[1].Text = Tintongtien().ToString("#,##0");
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            foreach (Button btn in flowLayoutPanel1.Controls)
+            {
+                if (btn.BackColor == Color.Yellow)
+                {
+
+                    btn.BackColor = Color.Gray;
+                    
+                }
+
+            }
+            txtGia.Text = "";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            statusStrip1.Items[3].Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
